b92ee5d9c6872c00b9b7fe44fa9c2e4a9714470f
tomonnar <tobias.monnard@epfl.ch>
2026-05-11 11:08:37 +0200

    Update Script to produce ell1pi samples

--- examples/FCCee/flavour/Bs2TauTau/ell1pi/analysis_stage1_bkg.py
+++ examples/FCCee/flavour/Bs2TauTau/ell1pi/analysis_stage1_bkg.py
@@ -2,12 +2,12 @@
 
 # list of samples to process
 processList_full = {
-    #'p8_ee_Zbb_ecm91':{'chunks':10},
-    #'p8_ee_Zcc_ecm91':{'chunks':10},
-    #'p8_ee_Zss_ecm91':{'chunks':10},
-    #'p8_ee_Zud_ecm91':{'chunks':10},
+    'p8_ee_Zbb_ecm91':{'chunks':10,'fraction':2e-3},
+    'p8_ee_Zcc_ecm91':{'chunks':10,'fraction':2e-3},
+    'p8_ee_Zss_ecm91':{'chunks':10,'fraction':2e-3},
+    'p8_ee_Zud_ecm91':{'chunks':10,'fraction':2e-3},
     #'p8_ee_Zbb_ecm91_EvtGen_Bs2TauTauTAUHADNU':{'chunks':10},
-    'p8_ee_Zbb_ecm91_EvtGen_Bs2TauTau':{'chunks':20,'fraction':1.0},
+    #'p8_ee_Zbb_ecm91_EvtGen_Bs2TauTau':{'chunks':10,'fraction':0.1},
 }
 
 #processList_full = {
@@ -238,7 +238,7 @@ class RDFanalysis():
                ##      Select Bs->Tau(->ell)Tau(->1pi)    ##
                #############################################
                #IF SIGNAL => FILTER
-               .Filter("(n_genBs2TauTau_Tauminus_1pi > 0 && n_genBs2TauTau_Tauplus_ell > 0) || (n_genBs2TauTau_Tauplus_1pi > 0 && n_genBs2TauTau_Tauminus_ell > 0)") 
+               #.Filter("(n_genBs2TauTau_Tauminus_1pi > 0 && n_genBs2TauTau_Tauplus_ell > 0) || (n_genBs2TauTau_Tauplus_1pi > 0 && n_genBs2TauTau_Tauminus_ell > 0)") 
                
                #############################################
                ##              Build Reco Vertex          ##
