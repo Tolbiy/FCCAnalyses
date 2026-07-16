ff69ab1dec5ce0136ab15e96d7729b68feb4bceb
tomonnar <tobias.monnard@epfl.ch>
2026-07-16 11:56:29 +0200

    1pi1pi: Updated number of chunks

--- examples/FCCee/flavour/Bs2TauTau/1pi1pi/analysis_stage1_bkg.py
+++ examples/FCCee/flavour/Bs2TauTau/1pi1pi/analysis_stage1_bkg.py
@@ -2,10 +2,10 @@
 
 # list of samples to process
 processList_full = {
-    'p8_ee_Zbb_ecm91':{'chunks':10},
-    'p8_ee_Zcc_ecm91':{'chunks':10},
-    'p8_ee_Zss_ecm91':{'chunks':10},
-    'p8_ee_Zud_ecm91':{'chunks':10},
+    'p8_ee_Zbb_ecm91':{'chunks':100},
+    'p8_ee_Zcc_ecm91':{'chunks':100},
+    'p8_ee_Zss_ecm91':{'chunks':100},
+    'p8_ee_Zud_ecm91':{'chunks':100},
     #'p8_ee_Zbb_ecm91_EvtGen_Bs2TauTauTAUHADNU':{'chunks':10},
     #'p8_ee_Zbb_ecm91_EvtGen_Bs2TauTau':{'chunks':10,'fraction':0.1},
 }
